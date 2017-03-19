5f8e968798dfc5a0bc57814b69022fe908d59295
nanyr <nanyramirez0@gmail.com>
2017-03-19 14:54:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+let counter=0;
+  function addBump(){
+    counter+=1;
+  };
+
+  function getBumps(){
+    return counter;
+  };
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+
+
+function createAnimal(animalType){
+  function innerFunction(deadlyDevice){
+    return {
+      animalType: animalType,
+      deadlyDevice: deadlyDevice
+    }
+  }
+  return innerFunction;
+}
+
+
+const sharkCreator= createAnimal('Shark');
+const sharkWithFrickinLaserbeam= sharkCreator('Laserbeam');
+const sharkWithFrickinCannon= sharkCreator('Cannon');
